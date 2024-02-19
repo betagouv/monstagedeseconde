953cd809fdafd8b53a82ecd5b5bbb728d3693306
Etienne Weil <weil.etienne@hotmail.fr>
2024-02-19 14:18:37 +0100

    fix(tests): mostly while footer is changing

--- test/controllers/footer_test.rb
+++ test/controllers/footer_test.rb
@@ -7,12 +7,12 @@ class FooterTest < ActionDispatch::IntegrationTest
   test 'presence of footer links within valid rg2a footer' do
     get root_path
 
-    assert_select('a[href=?]',
-                  'https://incubateur.anct.gouv.fr/')
+    # TODO bring content back
+    # assert_select('a[href=?]', 'https://incubateur.anct.gouv.fr/')
     assert_select('a[href=?]', mentions_legales_path)
-    assert_select('a[href=?]', conditions_d_utilisation_path)
-    assert_select('a[href=?]', accessibilite_path)
-    assert_select('a[href=?]', accessibilite_path)
+    assert_select('a[href=?]', politique_de_confidentialite_path)
+    # assert_select('a[href=?]', conditions_d_utilisation_path)
+    # assert_select('a[href=?]', accessibilite_path)
 
   end
 end
