c0335a5c681aeab06c5d506fa6c386fc5d36ed85
Max <max@MacBook-Pro-de-Max.local>
2024-02-21 10:55:07 +0100

    Fix deploy script

--- infra/staging/deploy.sh
+++ infra/staging/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -x
-
+set -a
+source .env
+set +a
 target='staging'
 git remote -vvv | grep $target | grep 'clever'
 
 if [ ! $? -eq 0 ]; then
   echo "missing git remote $target"
   echo "please add $target repo"
-  echo "-> git remote add $target $ENV['CLEVER_GIT_STAGING_URL']"
+  echo "-> git remote add $target $CLEVER_GIT_STAGING_URL
   exit 1;
 fi
 
