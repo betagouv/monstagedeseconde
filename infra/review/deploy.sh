328ad111d90d60542478bf7008233f6bc8196dcc
Max <maxime.pierrot@gmail.com>
2024-03-07 10:29:31 +0100

    Update infra scripts

--- infra/review/deploy.sh
+++ infra/review/deploy.sh
@@ -7,7 +7,7 @@ git remote -vvv | grep $target | grep 'clever'
 if [ ! $? -eq 0 ]; then
   echo "missing git remote $target"
   echo "please add $target repo"
-  echo "-> git remote add $target $CLEVER_GIT_STAGING_URL"
+  echo "-> git remote add $target $CLEVER_GIT_REVIEW_URL"
   exit 1;
 fi
 
