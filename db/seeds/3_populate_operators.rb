466dca76c4f7c9e141088fee34240dd22d828315
Etienne Weil <weil.etienne@hotmail.fr>
2024-03-13 17:58:34 +0100

    fix(seed): seed fails when missing operator. Now added

--- db/seeds/3_populate_operators.rb
+++ db/seeds/3_populate_operators.rb
@@ -1,4 +1,7 @@
 def populate_operators
+  Operator.find_or_create_by!(name: "JobIRL",
+                              website: "",
+                              target_count: 32)
 end
 
 call_method_with_metrics_tracking([:populate_operators])
