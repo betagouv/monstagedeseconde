315e1dd92adfc9481867d2a3c5c22d8aeaa7502e
Max <maxime.pierrot@gmail.com>
2024-03-20 15:57:20 +0100

    Create agreement in any cases

--- test/controllers/api/internship_offers/update_test.rb
+++ test/controllers/api/internship_offers/update_test.rb
@@ -159,7 +159,7 @@ module Api
         }
       )
       assert_response :success
-      assert_in_delta Time.now.to_i , @internship_offer.reload.published_at.to_i, 0.001
+      assert_in_delta Time.now.to_i , @internship_offer.reload.published_at.to_i, 0.002
       assert_equal true, @internship_offer.published?
     end
   end
