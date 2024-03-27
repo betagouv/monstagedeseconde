72377360eaccccad683c67860deb8a1a9f695bfd
Etienne WEIL <fitchMitch@users.noreply.github.com>
2024-03-27 11:26:11 +0100

    feat(banner): more adds to come (#146)

--- app/front_assets/components/InternshipOfferResults.jsx
+++ app/front_assets/components/InternshipOfferResults.jsx
@@ -148,7 +148,7 @@ const InternshipOfferResults = ({ count, sectors, params }) => {
       <div className="row mx-0 fr-px-2w no-x-scroll">
         <div className={`${isMobile() ? 'col-12 px-0' : 'col-sm-12 px-3'} d-flex flex-row-reverse no-x-scroll`} style={{ overflowY: 'scroll' }}>
           <div className="container-monstage">
-            <div className="results-col results-row no-x-scroll hide-scrollbar fr-mt-2w fr-mx-3w">
+            <div className="results-col results-row no-x-scroll hide-scrollbar fr-mt-2w fr-mx-lg-0 fr-mx-1w ">
               <div className="row fr-py-2w mx-0 ">
                 <div className="col-8 px-0">
                   {
