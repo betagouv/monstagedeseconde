ebe640e1290e3ba8ce54172f53b1ccffc702a10e
Max <maxime.pierrot@gmail.com>
2024-03-27 15:06:01 +0100

    Add offers examples

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,28 +9,11 @@ class PagesController < ApplicationController
                               statistician_landing
                               student_landing]
 
-  before_action :last_three_offers, only: %i[home student_landing]
-  # before_action :last_three_offers, only: %i[home pro_landing student_landing school_management_landing]
-
   def register_to_webinar
     authorize! :subscribe_to_webinar, current_user
     current_user.update(subscribed_to_webinar_at: Time.zone.now)
     redirect_to WEBINAR_URL,
                 allow_other_host: true
-  end
-
-  def flyer
-    respond_to do |format|
-      format.html
-      format.pdf do
-        send_data(
-          File.read(Rails.root.join("public", "MS3_Flyer_2022.pdf")),
-          filename: "MS3E_flyer_2022.pdf",
-          type: 'application/pdf',
-          disposition: 'inline'
-        )
-      end
-    end
   end
 
   def offers_with_sector
@@ -42,13 +25,4 @@ class PagesController < ApplicationController
 
   alias_method :school_management_landing, :student_landing
   alias_method :statistician_landing, :student_landing
-
-  private
-
-  def last_three_offers
-    @internship_offers = []
-    if offers_with_sector.count > 2
-      @internship_offers = offers_with_sector.last(3)
-    end
-  end
 end
