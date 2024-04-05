c045ca3b4d145f6952e684251961d956ac311c6e
Max <maxime.pierrot@gmail.com>
2024-04-05 14:57:44 +0200

    RM migration

--- db/migrate/20240405094938_add_department_to_schools.rb
+++ db/migrate/20240405094938_add_department_to_schools.rb
@@ -1,10 +1,10 @@
 class AddDepartmentToSchools < ActiveRecord::Migration[7.1]
   def change
     add_reference :schools, :department, foreign_key: true, index: true
-    TaskManager.new(
-      allowed_environments: %w[development test review production staging],
-      task_name: 'migrations:fullfill_schools_department',
-      arguments: ['once_str, only_str']
-    ).play_task_once(run_with_a_job: true)
+    # TaskManager.new(
+    #   allowed_environments: %w[development test review production staging],
+    #   task_name: 'migrations:fullfill_schools_department',
+    #   arguments: ['once_str, only_str']
+    # ).play_task_once(run_with_a_job: true)
   end
 end
--- db/migrate/20240405101512_remove_old_department_from_schools.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOldDepartmentFromSchools < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :schools, :department, :string
-  end
-end
