57a75bd82962e763084e283f001680936ac1cbbd
Etienne Weil <weil.etienne@hotmail.fr>
2024-04-09 15:12:44 +0200

    fix(tests): external api calls still allowed on communes API

--- app/controllers/api_city_proxy_controller.rb
+++ app/controllers/api_city_proxy_controller.rb
@@ -2,18 +2,12 @@
 # not the neciest solution, but safest
 class ApiCityProxyController < ApplicationController
   def search
-    time_start = Time.now
     cache = Rails.cache
     cache_key = { autocomplete_city: permitted_params }
     cache.fetch(cache_key) do
       api_request
     end
     returned_value = cache.fetch(cache_key)
-    time_end = Time.now
-    puts '================================'
-    puts "Elapsed time for city caching #{permitted_params[:nom]} - #{permitted_params[:code]}: #{(time_end - time_start) * 1000} ms"
-    puts '================================'
-    puts ''
     render json: returned_value[:json], status: returned_value[:code]
   end
 
