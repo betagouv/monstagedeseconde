5b504f81f15d7775eb003f0cfc42f276f84a2128
Etienne Weil <weil.etienne@hotmail.fr>
2024-04-12 13:34:37 +0200

    feat(search): coded craft now using pgSearch

--- app/models/coded_craft.rb
+++ app/models/coded_craft.rb
@@ -1,7 +1,21 @@
 class CodedCraft < ApplicationRecord
+  include PgSearch::Model
+  
   belongs_to :detailed_craft
   has_many :coded_craft_fields, dependent: :destroy, inverse_of: :coded_craft
 
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :ogr_code, presence: true, length: { maximum: 10 }, uniqueness: true
+
+  pg_search_scope :search_by_keyword,
+                  against: :name,
+                  ignoring: :accents,
+                  using: {
+                    tsearch: {
+                      dictionary: 'public.config_search_keyword',
+                      tsvector_column: 'name_tsv',
+                      prefix: true
+                    }
+                  }
+
 end
