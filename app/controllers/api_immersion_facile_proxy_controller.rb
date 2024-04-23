df38bc342efc2e095307c2d6817400ed7cda08de
Etienne Weil <weil.etienne@hotmail.fr>
2024-04-23 10:32:50 +0200

    feat(call to Services::ImmersionFacile) to have results shown in index

--- app/controllers/api_immersion_facile_proxy_controller.rb
+++ app/controllers/api_immersion_facile_proxy_controller.rb
@@ -2,9 +2,9 @@
 
 # ad blockers can block API, so we proxy our calls to it.
 # not the neciest solution, but safest
-class ApiInsertionFacileProxyController < ApplicationController
+class ApiImmersionFacileProxyController < ApplicationController
   def search
-    render json: Api::AutocompleteInsertionFacile.search(
+    render json: Api::AutocompleteImmersionFacile.search(
       params: params.permit(:city, :radius, :keyword)
     )
   end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,72 +2,36 @@ class CompaniesController < ApplicationController
   layout 'search'
 
   def index
-    @companies = [
-      OpenStruct.new(
-        name: 'Oracle',
-        job: 'Conseiller offres internet',
-        sector: 'Télécommunication sans fil',
-        size: '+500 salariés',
-        address: ' 25 rue de la gare, 75001 Paris',
-        link: 'https://www.oracle.com/fr/index.html'
-      ),
-      OpenStruct.new(
-        name: 'Google',
-        job: 'Développeur web',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '8 rue de Londres, 75009 Paris',
-        link: 'https://www.google.com/'
-      ),
-       OpenStruct.new(
-        name: 'Microsoft',
-        job: 'Ingénieur informatique',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '39 quai du Président Roosevelt, 92130 Issy-les-Moulineaux',
-        link: 'https://www.microsoft.com/fr-fr'
-      ),
-       OpenStruct.new(
-        name: 'Apple',
-        job: 'Développeur mobile',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '7 place d\'Iéna, 75016 Paris',
-        link: 'https://www.apple.com/fr/'
-      ),
-       OpenStruct.new(
-        name: 'Facebook',
-        job: 'Community manager',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '6 rue Ménars, 75002 Paris',
-        link: 'https://fr-fr.facebook.com/'
-      ),
-       OpenStruct.new(
-        name: 'Amazon',
-        job: 'Développeur web',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '67 boulevard du Général Leclerc, 92110 Clichy',
-        link: 'https://www.amazon.fr/'
-      ),
-       OpenStruct.new(
-        name: 'IBM',
-        job: 'Ingénieur informatique',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '17 avenue de l\'Europe, 92275 Bois-Colombes',
-        link: 'https://www.ibm.com/fr-fr'
-      ),
-       OpenStruct.new(
-        name: 'Twitter',
-        job: 'Community manager',
-        sector: 'Technologie de l\'information',
-        size: '+500 salariés',
-        address: '78 rue Taitbout, 75009 Paris',
-        link: 'https://twitter.com/?lang=fr'
-       )
-    ]
-    @pages = InternshipOffer.all.order(created_at: :desc).page(params[:page]).per(3)
+    @city_coordinates = Geofinder.coordinates(search_params[:city])
+    return [] if @city_coordinates.empty?
+
+    @latitude = @city_coordinates.first
+    @longitude = @city_coordinates.last
+    @radius_in_km = search_params[:radius_in_km].presence || 10
+
+    service = Services::ImmersionFacile.new({ latitude: latitude,
+                                              longitude: longitude,
+                                              radius_in_km: radius_in_km })
+    @companies = service.perform
+    @pages = InternshipOffer.all
+                            .order(created_at: :desc)
+                            .page(params[:page])
+                            .per(3)
+    render :index
+  end
+
+  private
+
+  def prefered_keys = []
+
+  attr_accessor :latitude, :longitude, :radius_in_km
+  attr_reader :city, :keyword
+
+  def search_params
+    params.permit(:city,
+                  :latitude,
+                  :longitude,
+                  :radius_in_km,
+                  :keyword)
   end
 end
