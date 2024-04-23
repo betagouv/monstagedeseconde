36d9c6c18394b115b2a00cd0b5a985bff0b8b721
Etienne Weil <weil.etienne@hotmail.fr>
2024-04-23 11:03:21 +0200

    feat(ui): company: presenter : url IF

---
+++ app/libs/presenters/company.rb
@@ -0,0 +1,24 @@
+module Presenters
+  class Company
+
+    def immersion_facilitee_url
+      params_hash = {
+        siret: siret,
+        location: location_id,
+        appellationCode: appellation_code
+      }
+      "https://immersion-facile.beta.gouv.fr/offre?#{params_hash.to_query}"
+    end
+
+    attr_reader :company, :siret, :appellation_code, :location_id
+
+    private
+
+    def initialize(company)
+      @company = company
+      @siret = company['siret']
+      @appellation_code = company['appellations'][0]['appellationCode']
+      @location_id = company['locationId']
+    end
+  end
+end
