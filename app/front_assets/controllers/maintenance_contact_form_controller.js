7c79308d15361d1417f9f3581d2d35ddba3aa042
Etienne Weil <weil.etienne@hotmail.fr>
2024-06-27 16:48:48 +0200

    fix(code): cleaning extra lines

--- app/front_assets/controllers/maintenance_contact_form_controller.js
+++ app/front_assets/controllers/maintenance_contact_form_controller.js
@@ -9,13 +9,13 @@ export default class extends Controller {
     'submitButton'
   ];
   submitForm(event) {
+    const recipient = "contact@stagedeseconde.education.gouv.fr"
     event.preventDefault();
     const name = this.nameFieldTarget.value;
     const email = this.emailFieldTarget.value;
     const message = this.messageFieldTarget.value;
-    const recipient = "contact@stagedeseconde.education.gouv.fr"
     window.open(
-      `mailto:${recipient}?Reply-To=${email}&Sender=${email}&subject=Contact&body=${message}%20-%20Message%20de%20${name}`
+      `mailto:${recipient}?Reply-To=${email}&From=${email}&subject=Contact&body=${message}%20-%20Message%20de%20${name}`
     )
   }
 
