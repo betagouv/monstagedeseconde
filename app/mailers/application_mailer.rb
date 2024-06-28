9c0772efb88bcf2e6e888e9522197809785baa32
Etienne Weil <weil.etienne@hotmail.fr>
2024-06-28 17:56:28 +0200

    feat(reply_to): maintenance message

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -14,12 +14,13 @@ class ApplicationMailer < ActionMailer::Base
     @site_url ||= root_url.html_safe
   end
 
-  def send_email(to:, subject:, cc: nil, specific_layout: nil)
+  def send_email(to:, subject:, cc: nil, specific_layout: nil, reply_to: nil)
     Rails.logger.error("mail without recipient sending attempt. " \
                        "Subject: #{subject}") and return if to.blank?
-    
-    params = { to: to, subject: subject }
-    params.merge!(cc: cc) unless cc.nil?
+
+    params = { to:, subject: }
+    params.merge!(cc:) unless cc.nil?
+    params.merge!(reply_to:) unless reply_to.nil?
     mail(**params) and return if specific_layout.nil?
 
     mail(**params) do |format|
