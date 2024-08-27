991f29e894029f0604bb0ee1ef87f9b36dfd1cde
Max <maxime.pierrot@gmail.com>
2024-08-27 16:43:28 +0200

    Add dynamic resources for pros

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,6 +28,7 @@ class PagesController < ApplicationController
 
   def pro_landing
     @faqs = get_faqs('pro')
+    @resources = get_resources('pro')
   end
 
   def school_management_landing
@@ -85,6 +86,22 @@ class PagesController < ApplicationController
     serialize_faq(response.results)
   end
 
+  def get_resources(tag)
+    api = Prismic.api(ENV['PRISMIC_URL'], ENV['PRISMIC_API_KEY'])
+
+    begin
+      response = api.query([
+                             Prismic::Predicates.at('document.type', 'resource'),
+                             Prismic::Predicates.at('document.tags', [tag])
+                           ])
+    rescue StandardError => e
+      puts "Error: #{e}"
+      return
+    end
+
+    serialize_resource(response.results)
+  end
+
   def serialize_faq(results)
     results.map do |doc|
       {
@@ -92,5 +109,26 @@ class PagesController < ApplicationController
         answer: doc['faq.answer'].as_html(link_resolver)
       }
     end
+  end
+
+  def serialize_resource(results)
+    # Group by school level
+    grouped_by_school = results.group_by { |doc| doc['resource.school_level'].as_text }
+
+    grouped_by_school.transform_values! do |docs|
+      # Group by category
+      grouped_by_category = docs.group_by { |doc| doc['resource.category'].as_text }
+
+      grouped_by_category.transform_values! do |category_docs|
+        category_docs.map do |doc|
+          {
+            url: doc.href,
+            title: doc['resource.title'].as_text
+          }
+        end
+      end
+    end
+
+    grouped_by_school
   end
 end
