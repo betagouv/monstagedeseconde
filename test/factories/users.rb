ef24ce772060fc0e1d129bfcca4d521a1d5e4822
Max <maxime.pierrot@gmail.com>
2024-09-17 17:15:53 +0200

    remove gradin

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -27,7 +27,6 @@ FactoryBot.define do
       gender { %w[m f].sample }
       birth_date { 14.years.ago }
       school { create(:school, :with_school_manager) }
-      gradin { 'troisieme' }
       address { FFaker::AddressFR.full_address }
       legal_representative_email { FFaker::Internet.email }
       legal_representative_full_name { FFaker::NameFR.name }
