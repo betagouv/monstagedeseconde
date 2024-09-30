33c1507ef017f2e60ca6c1a249097d7de1cd1349
Max <maxime.pierrot@gmail.com>
2024-09-30 11:20:16 +0200

    fix tests

--- test/system/devise/sign_up_students_test.rb
+++ test/system/devise/sign_up_students_test.rb
@@ -67,7 +67,7 @@ class SignUpStudentsTest < ApplicationSystemTestCase
     fill_in 'Adresse électronique', with: new_email
     fill_in 'Mot de passe', with: new_email_password
     click_button 'Se connecter'
-    find('.h4 .strong', text: 'Les offres de stage')
+    find('.h4 .strong', text: "Rechercher un stage d'observation")
     assert_select '#alert-text', count: 0
   end
 
