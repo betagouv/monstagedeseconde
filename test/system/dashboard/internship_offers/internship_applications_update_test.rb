c2d2f9166a7c1cd189deaeec1f0cee0ea9e838b4
Max <maxime.pierrot@gmail.com>
2024-10-30 08:29:15 +0100

    Fix tests

--- test/system/dashboard/internship_offers/internship_applications_update_test.rb
+++ test/system/dashboard/internship_offers/internship_applications_update_test.rb
@@ -106,6 +106,8 @@ module Dashboard::InternshipOffers
         end
         # fill_in 'Numéro de portable élève ou responsable légal',	with: "0600060606"
         click_on 'Valider'
+        click_on 'Envoyer ma candidature'
+
         assert_equal 2, InternshipApplication.count
         other_internship_application = InternshipApplication.last
         sign_out(other_student)
