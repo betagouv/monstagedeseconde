dc231b2bc1cec5dfe41522f9e2e09aee04a2d90a
Etienne Weil <weil.etienne@hotmail.fr>
2024-11-21 10:44:29 +0100

    fix(deployment) : table creation if not exists allready : internship_offer_grade

--- db/migrate/20241009091909_create_planning_grades_table.rb
+++ db/migrate/20241009091909_create_planning_grades_table.rb
@@ -7,6 +7,8 @@ class CreatePlanningGradesTable < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
+    return if table_exists?(:internship_offer_grades)
+
     create_table :internship_offer_grades do |t|
       t.references :grade, null: false, foreign_key: true
       t.references :internship_offer, null: false, foreign_key: true
