c8f235289127a4760c54abb43e65693dec17191c
Etienne Weil <weil.etienne@hotmail.fr>
2024-11-21 11:30:54 +0100

    fix(migration): adding and removing entreprise_coordinates are now commented to be created the right way once only

--- db/migrate/20241024075951_remove_entreprise_coordinates_from_internship_offer.rb
+++ db/migrate/20241024075951_remove_entreprise_coordinates_from_internship_offer.rb
@@ -1,5 +1,5 @@
 class RemoveEntrepriseCoordinatesFromInternshipOffer < ActiveRecord::Migration[7.1]
   def change
-    remove_column :internship_offers, :entreprise_coordinates, :point
+    # remove_column :internship_offers, :entreprise_coordinates, :point
   end
 end
