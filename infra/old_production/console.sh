2abd7dd124301d637db66dd667ac1b1618cd82ef
Etienne Weil <weil.etienne@hotmail.fr>
2025-01-21 17:52:45 +0100

    feat(redirection):

---
+++ infra/old_production/console.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+set -a
+source .env
+set +a
+
+SSH_PRIV=~/.ssh/clevercloud-monstage
+if [ ! -f "$SSH_PRIV" ]; then
+  echo "missing private key to push, check kdbx for content"
+  exit 1;
+fi;
+
+ssh -t ssh@sshgateway-clevercloud-customers.services.clever-cloud.com $CLEVER_APP_PROD_ID
---
+++ infra/old_production/link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+
+clever link app_cb8fb836-b0c7-43e1-ba2e-130a73626fa6
+clever link app_7a789cbd-96eb-4177-a800-42163359444d
