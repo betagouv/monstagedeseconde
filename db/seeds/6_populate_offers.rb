db4b1818221f3f654894d2b4ae750a12872efec3
Etienne Weil <weil.etienne@hotmail.fr>
2025-01-23 15:42:37 +0100

    fix(seed): missing line added

--- db/seeds/6_populate_offers.rb
+++ db/seeds/6_populate_offers.rb
@@ -364,6 +364,7 @@ def populate_internship_offers
   )
 
   # 13
+  current_year = SchoolTrack::Seconde.current_year.to_i
   former_year = current_year - 1
   weeks = SchoolTrack::Seconde.both_weeks(year: former_year)
   InternshipOffers::WeeklyFramed.create!(
@@ -417,8 +418,6 @@ def populate_internship_offers
   )
 
   # 15 older school year
-  current_year = SchoolTrack::Seconde.current_year
-  former_year = current_year - 1
   weeks = SchoolTrack::Seconde.both_weeks(year: former_year)
   weeks += Week.of_previous_school_year
   InternshipOffers::Api.create!(
