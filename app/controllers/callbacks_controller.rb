5d7f678fe294af7b34c4c3d8740ef88edb9e1c4a
Max <maxime.pierrot@gmail.com>
2025-01-26 21:52:05 +0100

    Educonnect signin

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -83,8 +83,11 @@ class CallbacksController < ApplicationController
     end
 
     Rails.logger.info("Student confirmed at: #{student.confirmed_at}")
-    
+
     sign_in(student)
+    
+    Rails.logger.info("Student signed in")
+
     redirect_to after_sign_in_path_for(student), notice: 'Vous êtes bien connecté'
   end
 
