a42e8e612845b20be9d77728fd28f664535eb2a4
Etienne Weil <weil.etienne@hotmail.fr>
2025-02-01 14:20:47 +0100

    fix(500): operators are allowed to access their home page but their statistitics

--- config/routes.rb
+++ config/routes.rb
@@ -237,16 +237,14 @@ Rails.application.routes.draw do
     # ------------------ DASHBOARD END ------------------
   end
   # ------------------ SCOPE END ------------------
-  unless when_employers_only
-    namespace :reporting, path: 'reporting' do
-      get '/dashboards', to: 'dashboards#index'
+  namespace :reporting, path: 'reporting' do
+    get '/dashboards', to: 'dashboards#index'
 
-      get '/schools', to: 'schools#index'
-      get '/employers_internship_offers', to: 'internship_offers#employers_offers'
-      get 'internship_offers', to: 'internship_offers#index'
-      get 'operators', to: 'operators#index'
-      put 'operators', to: 'operators#update'
-    end
+    get '/schools', to: 'schools#index'
+    get '/employers_internship_offers', to: 'internship_offers#employers_offers'
+    get 'internship_offers', to: 'internship_offers#index'
+    get 'operators', to: 'operators#index'
+    put 'operators', to: 'operators#update'
   end
 
   get 'api_address_proxy/search', to: 'api_address_proxy#search', as: :api_address_proxy_search
