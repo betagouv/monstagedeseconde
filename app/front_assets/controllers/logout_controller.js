4b022498a61a0865311f54885ffe8b54e4db65cb
Max <maxime.pierrot@gmail.com>
2025-02-06 15:08:22 +0100

    Fim fix

--- app/front_assets/controllers/logout_controller.js
+++ app/front_assets/controllers/logout_controller.js
@@ -5,15 +5,21 @@ export default class extends Controller {
 
   connect() {
     try {
-      const educonnectLogout = this.element.querySelector('.educonnect-logout')
+      const educonnectLogout = this.element.querySelector('.educonnect-logout');
+      const fimLogout = this.element.querySelector('.fim-logout');
+  
       if (educonnectLogout) {
-        console.log('Déconnexion EduConnect trouvée, tentative de déconnexion...')
-        window.location.href = educonnectLogout.href
+        console.log('Déconnexion EduConnect trouvée, tentative de déconnexion...');
+        window.location.href = educonnectLogout.href;
+      } else if (fimLogout) {
+        console.log('Déconnexion FIM trouvée, tentative de déconnexion...');
+        window.location.href = fimLogout.href;
       } else {
-        console.warn('L\'élément de déconnexion EduConnect n\'a pas été trouvé')
+        console.warn('L\'élément de déconnexion n\'a pas été trouvé');
       }
     } catch (error) {
-      console.error('Erreur lors de la déconnexion EduConnect:', error)
+      console.error('Erreur lors de la déconnexion :', error);
     }
   }
 }
+
