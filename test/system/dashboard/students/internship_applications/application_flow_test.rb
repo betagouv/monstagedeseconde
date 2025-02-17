35bcbceaea8057232ea8caa53f3f974e74ba233e
Etienne Weil <weil.etienne@hotmail.fr>
2025-02-17 16:58:46 +0100

    fix(period): it is shown, storeed and as 2 weeks or 1 week according to the offer weeks number

--- test/system/dashboard/students/internship_applications/application_flow_test.rb
+++ test/system/dashboard/students/internship_applications/application_flow_test.rb
@@ -6,7 +6,7 @@ module Dashboard
       include Devise::Test::IntegrationHelpers
       include TeamAndAreasHelper
 
-      test 'first and uniq test before submitting when email was missing' do
+      test 'student 2nde first and uniq test before submitting when email was missing' do
         school = create(:school, :with_school_manager)
         employer, internship_offer = create_employer_and_offer_2nde
         student = create(:student, :seconde, phone: '+2620625852585', school:, email: nil)
@@ -27,7 +27,7 @@ module Dashboard
         assert_equal student.phone, internship_application.student_phone
       end
 
-      test 'student_email is suggested from previous internship_applications' do
+      test 'student 2nde student_email is suggested from previous internship_applications' do
         skip 'failing test on CI but passing locally' if ENV.fetch('CI') == 'true'
         employer, internship_offer = create_employer_and_offer_2nde
         second_internship_offer = create(:weekly_internship_offer_2nde, employer:)
@@ -47,7 +47,7 @@ module Dashboard
         assert_equal former_student_email, form_student_value
       end
 
-      test 'student_phone is suggested from previous internship_applications' do
+      test 'student 2nde student_phone is suggested from previous internship_applications' do
         employer, internship_offer = create_employer_and_offer_2nde
         second_internship_offer = create(:weekly_internship_offer_2nde, employer:)
         former_student_phone = '+330623055441'
@@ -127,6 +127,68 @@ module Dashboard
           find('.fr-alert--error strong', text: 'Contact email')
           find('.fr-alert--error ', text: "n'est pas valide")
         end
+      end
+
+      test '2nde student 2 weeks long application is shown as 2 weeks long in his dashboard' do
+        employer, internship_offer = create_employer_and_offer_2nde
+        internship_offer.update!(weeks: Week.both_school_track_selectable_weeks)
+        school = create(:school, :lycee, :with_school_manager)
+        student = create(:student, :seconde, school: school, email: 'test@free.fr', phone: '+ 330620554411')
+        sign_in(student)
+        visit internship_offer_path(internship_offer)
+        all('.fr-btn', text: 'Postuler').first.click
+        find('#internship_application_motivation', visible: false).set('Le dev ça motive')
+        click_on 'Valider ma candidature'
+        click_on 'Envoyer ma candidature'
+        assert_text 'Disponible sur 2 semaines : 16 juin 2025 → 27 juin 2025'
+        find('a', text: 'Voir').click
+        assert_text 'Disponible sur 2 semaines : 16 juin 2025 → 27 juin 2025'
+      end
+
+      test "2nde student 2 weeks long application is shown as 2 " \
+           "weeks long both in the employer dashboard" do
+            employer, internship_offer = create_employer_and_offer_2nde
+            school = create(:school, :lycee, :with_school_manager)
+            student = create(:student, :seconde, school: school, email: 'test@free.fr', phone: '+330620554411')
+            internship_offer.update!(weeks: Week.both_school_track_selectable_weeks)
+            create(:internship_application, :both_june_weeks , :submitted, internship_offer:, student:)
+            sign_in(student)
+        sign_in(employer)
+        visit dashboard_candidatures_path
+        click_link 'Répondre'
+        find('.h5', text: "Informations sur l'élève")
+        assert_text 'Disponible sur 2 semaines : 16 juin 2025 → 27 juin 2025'
+      end
+
+      test "As student with 1 week long application, it is shown as 1 week long in student's dashboard" do
+        employer, internship_offer = create_employer_and_offer_2nde
+        internship_offer.update!(weeks: [SchoolTrack::Seconde.first_week])
+        school = create(:school, :lycee, :with_school_manager)
+        student = create(:student, :seconde, school: school, email: 'test@free.fr', phone: '+ 330620554411')
+        sign_in(student)
+        visit internship_offer_path(internship_offer)
+        all('.fr-btn', text: 'Postuler').first.click
+        find('#internship_application_motivation', visible: false).set('Le dev ça motive')
+        click_on 'Valider ma candidature'
+        click_on 'Envoyer ma candidature'
+        assert_text 'Disponible la semaine du 16 juin 2025 au 20 juin 2025'
+        find('a', text: 'Voir').click
+        assert_text 'Disponible la semaine du 16 juin 2025 au 20 juin 2025'
+      end
+
+      test "As employer with 1 week long application, it is shown as 1 " \
+           "week long both in the employer's dashboard" do
+            employer, internship_offer = create_employer_and_offer_2nde
+            school = create(:school, :lycee, :with_school_manager)
+            student = create(:student, :seconde, school: school, email: 'test@free.fr', phone: '+330620554411')
+            internship_offer.update!(weeks: [SchoolTrack::Seconde.first_week])
+            create(:internship_application, :first_june_week , :submitted, internship_offer:, student:)
+            sign_in(student)
+        sign_in(employer)
+        visit dashboard_candidatures_path
+        click_link 'Répondre'
+        find('.h5', text: "Informations sur l'élève")
+        assert_text 'Disponible la semaine du 16 juin 2025 au 20 juin 2025'
       end
     end
   end
