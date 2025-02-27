b9d5de20559ba78d6d56894d930ddb8dc7d9f258
Etienne Weil <weil.etienne@hotmail.fr>
2025-02-27 15:30:01 +0100

    feat(wip): version with stars and score

--- app/front_assets/controllers/stars_controller.js
+++ app/front_assets/controllers/stars_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "stimulus";
-import {showContainer, hideContainer, setValueById} from "../utils/dom";
-import ActionCable from 'actioncable';
+import { toggleContainer } from "../utils/dom";
+import ActionCable from "actioncable";
 export default class extends Controller {
   static targets = [
     "title",
@@ -10,40 +10,53 @@ export default class extends Controller {
     "star2",
     "star3",
     "star4",
-    "star5"
+    "star5",
+    "recommendationPanel",
+    "validationButton"
   ];
 
   onBlurDescriptionInput(event) {
     const description = event.target.value;
-    if ((description.length > 8) && ((description.length % 3) == 0)) {
-      this.evaluator.perform('score', {
+    if (description.length > 8 && description.length % 3 == 0) {
+      this.evaluator.perform("score", {
         description: description,
-        title: this.titleTarget.value || '',
-        uid: this.channelParams.uid
+        title: this.titleTarget.value || "",
+        uid: this.channelParams.uid,
       });
     }
   }
 
   showStars(score) {
-    this.star1Target.style.color = (score >= 1) ? 'orange' : 'black';
-    this.star2Target.style.color = (score >= 6) ? 'orange' : 'black';
-    this.star3Target.style.color = (score >= 10) ? 'orange' : 'black';
-    this.star4Target.style.color = (score >= 14) ? 'orange' : 'black';
-    this.star5Target.style.color = (score >= 18.5) ? 'orange' : 'black';
+    this.star1Target.style.color = score >= 1 ? "orange" : "black";
+    this.star2Target.style.color = score >= 6 ? "orange" : "black";
+    this.star3Target.style.color = score >= 10 ? "orange" : "black";
+    this.star4Target.style.color = score >= 14 ? "orange" : "black";
+    this.star5Target.style.color = score >= 18.5 ? "orange" : "black";
   }
 
   connect() {
     this.channelParams = {
       channel: "ScoreChannel",
-      uid: Array.from(crypto.getRandomValues(new Uint8Array(16)), byte => byte.toString(16).padStart(2, '0')).join(''),
+      uid: Array.from(crypto.getRandomValues(new Uint8Array(16)), (byte) =>
+        byte.toString(16).padStart(2, "0")
+      ).join(""),
     };
     this.wssClient = ActionCable.createConsumer("/cable");
     this.evaluator = this.wssClient.subscriptions.create(this.channelParams, {
       received: (data) => {
         const score = parseFloat(data.score, 10).toFixed(2);
-        this.scoreTarget.innerHTML = score;
+        this.scoreTarget.innerHTML = score + "/20";
         this.showStars(score);
-      },
+        const good_enough = (score > 12);
+        const panelClassList = this.recommendationPanelTarget.children[0].classList;
+        if (good_enough) {
+          panelClassList.add('fr-alert--success');
+          panelClassList.remove('fr-alert--info');
+        } else {
+          panelClassList.remove('fr-alert--success');
+          panelClassList.add('fr-alert--info');
+        }
+      }
     });
   }
 
