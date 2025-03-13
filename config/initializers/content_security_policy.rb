7798b170c0e4d309dbd6f8cbfa1f5ef5c79ca323
Etienne Weil <weil.etienne@hotmail.fr>
2025-03-13 18:23:20 +0100

    feat(csp for dev) : uncommenting config initializer csp file

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,23 +11,25 @@ Rails.application.configure do
     policy.img_src     :self, :https, :data
     policy.object_src  :none
     policy.script_src :self, :https, "'connect-src'", "'unsafe-inline'", lambda {
-    # Allow @vite/client to hot reload javascript changes in development
-#    policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+      # Allow @vite/client to hot reload javascript changes in development
+      if Rails.env.development?
+        policy.script_src(*policy.script_src, :unsafe_eval, "http://#{ViteRuby.config.host_with_port}")
+      end
 
-    # You may need to enable this in production as well depending on your setup.
-#    policy.script_src *policy.script_src, :blob if Rails.env.test?
+      # You may need to enable this in production as well depending on your setup.
+      #    policy.script_src *policy.script_src, :blob if Rails.env.test?
 
       ENV.fetch('MATOMO_URL', '').gsub(%r{/js/.*}, '')
     }
     policy.connect_src :self, :https
     # Allow @vite/client to hot reload changes in development
-#    policy.connect_src *policy.connect_src, "ws://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+    policy.connect_src(*policy.connect_src, "ws://#{ViteRuby.config.host_with_port}") if Rails.env.development?
 
-    policy.style_src   :self, :https, "'unsafe-inline'"
+    policy.style_src :self, :https, "'unsafe-inline'"
     # Allow @vite/client to hot reload style changes in development
-#    policy.style_src *policy.style_src, :unsafe_inline if Rails.env.development?
+    policy.style_src(*policy.style_src, :unsafe_inline) if Rails.env.development?
 
-    policy.frame_src   'https://plugins.crisp.chat', 'https://uneleveunstage.crisp.help', ENV['METABASE_SITE_URL']
+    policy.frame_src 'https://plugins.crisp.chat', 'https://uneleveunstage.crisp.help', ENV['METABASE_SITE_URL']
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
