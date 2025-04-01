70adca7bce746be827642ec7ed34da8b2df2d846
Etienne WEIL <fitchMitch@users.noreply.github.com>
2025-04-01 15:35:16 +0200

    MGF-937_limit edit in admin + ine for students (#488)
    * limit edit in admin + ine for students
    * fix(seed): every student has an ine

--- app/models/school.rb
+++ app/models/school.rb
@@ -115,37 +115,6 @@ class School < ApplicationRecord
       scopes %i[all with_manager without_manager]
     end
 
-    edit do
-      field :name
-      field :visible
-      field :rep_kind, :enum do
-        enum do
-          School::VALID_TYPE_PARAMS
-        end
-      end
-      field :qpv
-      field :code_uai
-
-      field :coordinates do
-        partial 'autocomplete_address'
-      end
-
-      field :class_rooms
-
-      field :street do
-        partial 'void'
-      end
-      field :zipcode do
-        partial 'void'
-      end
-      field :city do
-        partial 'void'
-      end
-      field :department do
-        partial 'void'
-      end
-    end
-
     show do
       field :name
       field :visible
