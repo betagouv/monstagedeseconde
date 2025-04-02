9689b847b8032201ff6c3e8db45747a018483d00
Max <maxime.pierrot@gmail.com>
2025-04-02 15:14:08 +0200

    Add PDF conversion for signature

--- app/front_assets/controllers/preview_image_controller.js
+++ app/front_assets/controllers/preview_image_controller.js
@@ -2,22 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["input", "preview"]
-  
 
   preview(event) {
     const input = event.target
     const previewContainer = document.querySelector('.fr-upload-preview')
     
     if (input.files && input.files[0]) {
-      const reader = new FileReader()
+      const file = input.files[0]
       
-      reader.onload = (e) => {
+      if (file.type === 'application/pdf') {
         if (previewContainer) {
-          previewContainer.innerHTML = `<img src="${e.target.result}" alt="Aperçu de la signature" class="img-fluid" style="max-height: 192px;">`
+          previewContainer.innerHTML = '<p class="fr-text--sm fr-mb-0">Aperçu non disponible pour les fichiers PDF</p>'
         }
+      } else {
+        const reader = new FileReader()
+        
+        reader.onload = (e) => {
+          if (previewContainer) {
+            previewContainer.innerHTML = `<img src="${e.target.result}" alt="Aperçu de la signature" class="img-fluid" style="max-height: 192px;">`
+          }
+        }
+        
+        reader.readAsDataURL(file)
       }
-      
-      reader.readAsDataURL(input.files[0])
     }
   }
 
