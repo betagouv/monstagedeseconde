944411da2d287b84b3fd1268a1c55b8d444d854a
Max <maxime.pierrot@gmail.com>
2025-06-11 10:51:39 +0200

    Fix tests

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -208,9 +208,9 @@ class AbilityTest < ActiveSupport::TestCase
       refute ability.can?(:see_reporting_associations, User)
       refute ability.can?(:see_reporting_enterprises, User)
 
-      refute(ability.can?(:edit, InternshipAgreement))
-      refute(ability.can?(:create, InternshipAgreement))
-      refute(ability.can?(:update, InternshipAgreement))
+      assert(ability.can?(:edit, InternshipAgreement))
+      assert(ability.can?(:create, InternshipAgreement))
+      assert(ability.can?(:update, InternshipAgreement))
 
       statistician = create(:statistician, agreement_signatorable: true)
       ability = Ability.new(statistician)
