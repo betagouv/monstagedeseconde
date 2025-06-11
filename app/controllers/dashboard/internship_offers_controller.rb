82cf3c7aaf4b37669419bf9b76ff43fb216afd77
Max <maxime.pierrot@gmail.com>
2025-06-11 18:22:50 +0200

    Fix troisieme weeks on edit

--- app/controllers/dashboard/internship_offers_controller.rb
+++ app/controllers/dashboard/internship_offers_controller.rb
@@ -74,7 +74,7 @@ module Dashboard
 
     def edit
       authorize! :update, @internship_offer
-      @available_weeks = Week.both_school_track_selectable_weeks
+      @available_weeks = Week.selectable_from_now_until_next_school_year # Week.both_school_track_selectable_weeks TODO: remove this in july 2025
       set_internship_offer_attributes(@internship_offer)
       @republish = true
       @duplication = false
