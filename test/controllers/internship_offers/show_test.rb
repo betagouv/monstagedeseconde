75b4c6d016bcddd07beb1fcd3ac53cc370473d90
Etienne Weil <weil.etienne@hotmail.fr>
2025-08-18 17:13:03 +0200

    fix(test): internship has schools reserved and not a single school reserved anymore

--- test/controllers/internship_offers/show_test.rb
+++ test/controllers/internship_offers/show_test.rb
@@ -155,12 +155,13 @@ module InternshipOffers
       travel_to Date.new(2023, 10, 1) do
         student = create(:student, school: create(:school))
         other_school = create(:school)
-        internship_offer = create(:weekly_internship_offer_3eme, school: other_school, max_candidates: 5)
+        internship_offer = create(:weekly_internship_offer_3eme, schools: [other_school], max_candidates: 5)
+
         sign_in(student)
         get internship_offer_path(internship_offer)
 
-        assert_match "Offre réservée à l'établissement", response.body
-        assert_match internship_offer.school.name, response.body
+        assert_match 'Offre réservée à l&#39;établissement', response.body
+        assert_match internship_offer.schools.first.name, response.body
         assert_select '#new_internship_application', 0
       end
     end
