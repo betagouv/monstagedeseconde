72993aaafc1af6bc9095fc5e114ab505ff849bac
Etienne WEIL <fitchMitch@users.noreply.github.com>
2025-10-21 14:57:15 +0200

    MGF-1247-fix(after product review from dgsco) (#646)
    * Signatures dashboard
    * Fix table signature
    * dsfr version update
    * feat(student signature): now ok with the flow.  signature in pdf file still missing
    * feat(student internship agreement signature): now in pdf
    * feat(strategy change): no login with the given token_access
    * fix(code): wording update : legal_representative ==> student_legal_representative
    * fix(test facility) : agreement cannot be finaliezd twice
    * fix(config): adding Rails.application.routes.default_url_options for review
    * fix(behaviour): student checks legal's representatives emails
    * fix(after product review): behaviour changes for legal representatives
    * fix(after product review):  student's representative emails just below his signature, now
    * fix(wording): after dgsco review
    * feat(after product review and degesco comments): signatures are //
    * fix(after product review): look and feel, wording
    ---------
    
    Co-authored-by: Max <maxime.pierrot@gmail.com>

---
+++ app/controllers/dashboard/students/internship_agreements_controller.rb
@@ -0,0 +1,106 @@
+module Dashboard::Students
+  class InternshipAgreementsController < ApplicationController
+    include EduconnectLogout
+    layout 'no_link_layout', only: %i[new]
+    before_action :authenticate_user!, only: %i[sign legal_representative_sign]
+    before_action :fetch_internship_agreement
+
+    def new
+      #anyone can access this page and layout
+      # but only with a valid access_token and if not already signed by legal representative
+      access_token = params[:access_token]
+      student_legal_representative_nr = params[:student_legal_representative_nr]
+      if @internship_agreement.signed_by_legal_representative?
+        render :new,
+               params: {
+                 student_id: @internship_agreement.student.id,
+                 uuid: params[:uuid]
+               } and return
+      end
+      # verification through access_token
+      @internship_agreement = InternshipAgreement.find_by(access_token: access_token)
+      if @internship_agreement.nil?
+        redirect_to root_path, alert: "Convention introuvable" and return
+      end
+      render :new, params: {
+        student_id: @internship_agreement.student.id ,
+        uuid: params[:uuid],
+        access_token: access_token,
+        student_legal_representative_nr: student_legal_representative_nr
+      }
+    end
+
+    def sign
+      authorize! :sign, @internship_agreement
+      if @internship_agreement.signed_by_student?
+        redirect_to dashboard_students_internship_applications_path(student_id: current_user.id),
+                  alert: 'Vous avez déjà signé cette convention de stage' and return
+      end
+      if @internship_agreement.may_sign? && current_user.student?
+        Signature.create!(internship_agreement: @internship_agreement,
+                          signatory_role: 'student',
+                          user_id: current_user.id,
+                          signatory_ip: request.remote_ip,
+                          signature_date: Time.now)
+        @internship_agreement.sign!
+        redirect_to dashboard_students_internship_applications_path(student_id: current_user.id),
+                    notice: 'Vous avez bien signé la convention de stage. Un email a été envoyé aux responsables légaux pour les inviter à signer la convention de stage.' and return
+      else
+        redirect_to dashboard_students_internship_applications_path(student_id: current_user.id),
+                    notice: 'La signature de la convention de stage n\'a pas pu être effectuée.' and return
+      end
+    rescue ActiveRecord::RecordNotFound
+      redirect_to dashboard_students_internship_applications_path(student_id: current_user.id),
+                  alert: 'Convention introuvable'
+    end
+
+    def legal_representative_sign
+      access_token = legal_representative_sign_internship_agreement_params[:access_token]
+      @internship_agreement = InternshipAgreement.find_by(access_token: access_token)
+      if @internship_agreement.nil?
+        redirect_to root_path, alert: 'Convention introuvable' and return
+      elsif @internship_agreement.signed_by_legal_representative? || access_token.nil?
+        alert_msg = "Le représentant légal #{@internship_agreement.student_legal_representative_full_name} " \
+                    "a déjà signé cette convention de stage"
+        redirect_to root_path, alert: alert_msg and return
+      else
+        authorize! :sign, @internship_agreement
+        Signature.create!(internship_agreement: @internship_agreement,
+                          signatory_role: 'student_legal_representative',
+                          user_id: current_user.id,
+                          student_legal_representative_full_name: representative_full_name(params[:student_legal_representative_nr]),
+                          signatory_ip: request.remote_ip,
+                          signature_date: Time.now)
+
+        @internship_agreement.sign! if @internship_agreement.may_sign?
+        @internship_agreement.access_token = nil
+        @internship_agreement.save
+        redirect_to new_dashboard_students_internship_agreement_path(uuid: @internship_agreement.uuid),
+                    notice: 'Vous avez bien signé la convention de stage' and return
+      end
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, alert: 'Convention introuvable' and return
+    end
+
+    private
+
+    def representative_full_name(nr)
+      if nr == '1'
+        @internship_agreement.student_legal_representative_full_name
+      elsif nr == '2'
+        @internship_agreement.student_legal_representative_2_full_name
+      else
+        ''
+      end
+    end
+
+    def legal_representative_sign_internship_agreement_params
+      params.require(:signature)
+            .permit(:uuid, :access_token, :student_id, :student_legal_representative_nr)
+    end
+
+    def fetch_internship_agreement
+      @internship_agreement ||= InternshipAgreement.find_by(uuid: params[:uuid])
+    end
+  end
+end
---
+++ test/controllers/dashboard/students/internship_agreements_controller_test.rb
@@ -0,0 +1,252 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Dashboard
+  module Students
+    class InternshipAgreementsControllerTest < ActionDispatch::IntegrationTest
+      include Devise::Test::IntegrationHelpers
+      include TeamAndAreasHelper
+
+      test 'student can sign internship agreement' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+
+        employer, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                   internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        sign_in(student)
+        get sign_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid, student_id: student.id)
+        # email is asynchronously sent when creating the signature
+        assert_redirected_to dashboard_students_internship_applications_path(student_id: student.id)
+        follow_redirect!
+        assert_select('.alert', text: 'Vous avez bien signé la convention de stage. Un email a été envoyé aux responsables légaux pour les inviter à signer la convention de stage. Fermer ×')
+        assert_equal 1, internship_agreement.reload.signatures.count
+        assert_equal 'student', internship_agreement.signatures.first.signatory_role
+        assert_equal 'signatures_started', internship_agreement.aasm_state
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'student cannot sign twice the internship agreement' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        employer, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+        internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :signed_by_student_only, internship_application: internship_application)
+        sign_in(student)
+        get sign_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid, student_id: student.id)
+        # email is asynchronously sent when creating the signature
+        assert_redirected_to dashboard_students_internship_applications_path(
+          student_id: student.id,
+        )
+        follow_redirect!
+        assert_select('.alert', text: 'Vous avez déjà signé cette convention de stage Fermer ×')
+        assert_equal 1, internship_agreement.reload.signatures.count
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test "legal representative can sign internship agreement" do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        student_token = student.to_sgid.to_s
+
+        e_, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                    internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        refute_nil internship_agreement.access_token
+        assert_not_nil internship_agreement.student_legal_representative_full_name
+        assert_not_nil internship_agreement.student_legal_representative_email
+        assert_not_nil internship_agreement.student_legal_representative_2_full_name
+        assert_not_nil internship_agreement.student_legal_representative_2_email
+        sign_in(student)
+        post legal_representative_sign_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid, student_id: student.id, student_legal_representative_nr: '1'),
+             params: {
+               signature: {
+                 uuid: internship_agreement.uuid,
+                 student_id: student.id,
+                 access_token: internship_agreement.access_token,
+                 student_legal_representative_full_name: internship_agreement.student_legal_representative_full_name
+               }
+             }
+        # email is asynchronously sent when creating the signature
+        assert_redirected_to new_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid)
+        follow_redirect!
+        assert_select('.fr-alert', text: "La convention de stage a déjà été signée par #{internship_agreement.student_legal_representative_full_name}")
+        assert_equal 1, internship_agreement.reload.signatures.count
+        assert_equal 'student_legal_representative', internship_agreement.signatures.first.signatory_role
+        assert_equal 'signatures_started', internship_agreement.aasm_state
+        last_signature = Signature.last
+        assert_equal last_signature.student_legal_representative_full_name, internship_agreement.student_legal_representative_full_name
+        assert_equal last_signature.user_id, student.id
+        assert_nil  internship_agreement.access_token
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'legal representative cannot sign twice the internship agreement' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        student_token = student.to_sgid.to_s
+
+        e_, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                    internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        refute_nil internship_agreement.access_token
+        assert_not_nil internship_agreement.student_legal_representative_full_name
+        assert_not_nil internship_agreement.student_legal_representative_email
+        sign_in(student)
+        post legal_representative_sign_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid, student_id: student.id, student_legal_representative_nr: '1'),
+             params: {
+               signature: {
+                 uuid: internship_agreement.uuid,
+                 student_id: student.id,
+                 access_token: internship_agreement.access_token,
+                 student_legal_representative_full_name: internship_agreement.student_legal_representative_full_name
+               }
+             }
+        assert_redirected_to new_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid)
+        follow_redirect!
+        assert_select('.fr-alert', text: "La convention de stage a déjà été signée par #{internship_agreement.student_legal_representative_full_name}")
+        assert_equal 1, internship_agreement.reload.signatures.count
+        assert_equal 'student_legal_representative', internship_agreement.signatures.first.signatory_role
+        assert_equal 'signatures_started', internship_agreement.aasm_state
+
+        post legal_representative_sign_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid, student_id: student.id),
+             params: {
+               signature: {
+                 uuid: internship_agreement.uuid,
+                 student_id: student.id,
+                 access_token: internship_agreement.access_token,
+                 student_legal_representative_full_name: internship_agreement.student_legal_representative_full_name
+               }
+             }
+        assert_redirected_to root_path
+        follow_redirect!
+        assert_select('.alert', text: "Le représentant légal #{internship_agreement.student_legal_representative_full_name} a déjà signé cette convention de stage Fermer ×")
+        assert_equal 1, internship_agreement.reload.signatures.count
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'legal representative cannot sign with invalid token' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        student_token = student.to_sgid.to_s
+
+        e_, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                    internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        refute_nil internship_agreement.access_token
+        assert_not_nil internship_agreement.student_legal_representative_full_name
+        assert_not_nil internship_agreement.student_legal_representative_email
+        sign_in(student)
+        post legal_representative_sign_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid, student_id: student.id),
+             params: {
+               signature: {
+                 uuid: internship_agreement.uuid,
+                 student_id: student.id,
+                 access_token: 'invalid_token',
+                 student_legal_representative_full_name: internship_agreement.student_legal_representative_full_name
+               }
+             }
+        assert_redirected_to root_path
+        follow_redirect!
+        assert_select('.alert', text: 'Convention introuvable Fermer ×')
+        assert_equal 0, internship_agreement.reload.signatures.count
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'should handle RecordNotFound on sign action' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+
+        employer, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                   internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        sign_in(student)
+        get sign_dashboard_students_internship_agreement_path(uuid: 'nonexistent', student_id: student.id)
+        assert_redirected_to root_path
+        follow_redirect!
+        assert_select('.alert', text: "Vous n'êtes pas autorisé à effectuer cette action. Fermer ×")
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'new' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        student_token = student.to_sgid.to_s
+
+        e_, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                    internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        refute_nil internship_agreement.access_token
+        assert_not_nil internship_agreement.student_legal_representative_full_name
+        assert_not_nil internship_agreement.student_legal_representative_email
+
+        get new_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid,
+                                                             access_token: internship_agreement.access_token,
+                                                             student_id: student.id)
+        assert_response :success
+        assert_select 'h1', text: "Espace de signature de la convention de stage destiné aux responsables légaux de #{student.presenter.full_name}"
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'new with invalid token' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        student_token = student.to_sgid.to_s
+
+        e_, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                    internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        refute_nil internship_agreement.access_token
+        assert_not_nil internship_agreement.student_legal_representative_full_name
+        assert_not_nil internship_agreement.student_legal_representative_email
+
+        get new_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid,
+                                                             access_token: 'invalid_token',
+                                                             student_id: student.id)
+        assert_redirected_to root_path
+        follow_redirect!
+        assert_select('.alert', text: 'Convention introuvable Fermer ×')
+      rescue StandardError => e
+        flunk "Exception raised: #{e.message}\n#{e.backtrace.join("\n")}"
+      end
+
+      test 'new when already signed by legal representative' do
+        school = create(:school, :with_school_manager)
+        student = create(:student, school: school)
+        student_token = student.to_sgid.to_s
+
+        e_, internship_offer = create_employer_and_offer_2nde
+        internship_application = create(:weekly_internship_application, :approved, student: student,
+                                                                                    internship_offer: internship_offer)
+        internship_agreement = create(:internship_agreement, :validated, internship_application: internship_application)
+        create(:signature, :student_legal_representative, internship_agreement: internship_agreement, user_id: student.id)
+        internship_agreement.sign!
+        assert_not_nil internship_agreement.student_legal_representative_full_name
+        assert_not_nil internship_agreement.student_legal_representative_email
+
+        get new_dashboard_students_internship_agreement_path(uuid: internship_agreement.uuid,
+                                                             access_token: internship_agreement.access_token,
+                                                             student_id: student.id)
+        assert_response :success
+        assert_select 'h1', text: "Espace de signature de la convention de stage destiné aux responsables légaux de #{student.presenter.full_name}"
+        assert_select '.fr-alert', text: "La convention de stage a déjà été signée par #{internship_agreement.student_legal_representative_full_name}"
+      end
+    end
+  end
+end
