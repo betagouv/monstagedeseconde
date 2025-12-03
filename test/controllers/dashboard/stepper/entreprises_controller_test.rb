72a35ce926e9813cc38a9d69f67c6597c146e693
pierrax <maxime.pierrot@gmail.com>
2025-12-03 15:16:15 +0100

    Update geocoder adress entreprise

--- test/controllers/dashboard/stepper/entreprises_controller_test.rb
+++ test/controllers/dashboard/stepper/entreprises_controller_test.rb
@@ -85,6 +85,8 @@ module Dashboard::Stepper
         }.to_json
       }
       stub_request(:get, 'https://nominatim.openstreetmap.org/search?accept-language=en&addressdetails=1&format=json&q=75001%20Paris').to_return(geocoder_response)
+      stub_request(:get, 'https://nominatim.openstreetmap.org/search?accept-language=en&addressdetails=1&format=json&q=75001,%20France').to_return(geocoder_response)
+      stub_request(:get, 'https://nominatim.openstreetmap.org/search?accept-language=en&addressdetails=1&format=json&q=75001%20Paris,%20,%2075001,%20France').to_return(geocoder_response)
 
       assert_difference('Entreprise.count') do
         post(
