5c49cfdd656dd4d5cdc314d7379ac568cb62e43a
pierrax <maxime.pierrot@gmail.com>
2025-12-22 15:25:10 +0100

    Fix coordinator adress

--- app/controllers/dashboard/multi_stepper/multi_coordinators_controller.rb
+++ app/controllers/dashboard/multi_stepper/multi_coordinators_controller.rb
@@ -74,6 +74,19 @@ module Dashboard::MultiStepper
     def set_computed_params
       @multi_coordinator.employer_chosen_name ||= multi_coordinator_params[:employer_name]
       @multi_coordinator.employer_chosen_address ||= multi_coordinator_params[:employer_address]
+  
+     
+      # get city from employer_chosen_address
+      if @multi_coordinator.employer_chosen_address.present?
+        results = Geocoder.search(@multi_coordinator.employer_chosen_address)
+        if results.present?
+          result = results.first
+          puts "result: #{result.inspect}"
+          @multi_coordinator.city = result.city
+          @multi_coordinator.zipcode = result.postal_code
+          @multi_coordinator.street = result.street_address || result.address
+        end
+      end
     end
 
     def sanitize_content
