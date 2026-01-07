bf131f537f38d9f987b2a8dcc7531787f8d35110
Etienne WEIL <fitchMitch@users.noreply.github.com>
2026-01-07 09:44:01 +0100

    fix(multi_internship_agreements): field organisation_representative_role (#728)
    It is a copy of employer.role. It is not mandatory anymore for multi_internship_agreements. It is to be validated on employer's forms. Role validations are reenforced at registration time

--- app/models/internship_agreements/mono_internship_agreement.rb
+++ app/models/internship_agreements/mono_internship_agreement.rb
@@ -5,9 +5,7 @@ module InternshipAgreements
 
 
     with_options if: :enforce_employer_validations? do
-      validates :organisation_representative_full_name,
-                :organisation_representative_role,
-                :date_range,
+      validates :date_range,
                 :tutor_role,
                 :entreprise_address,
                 presence: true
@@ -19,6 +17,10 @@ module InternshipAgreements
       validates :activity_scope, presence: true, length: { maximum: 1500 }
       validate :valid_working_hours_fields
       validate :at_least_daily_hours_or_weekly_hours
+
+      validates :organisation_representative_role,
+                presence: true,
+                length: { minimum: 3, maximum: 150 }
     end
   end
 end
