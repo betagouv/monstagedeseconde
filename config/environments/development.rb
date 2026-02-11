35c5023b5534a8082b6a305c01965ad5ea448017
Etienne WEIL <fitchMitch@users.noreply.github.com>
2026-02-11 10:33:12 +0100

    MGF-1497-feat(gem): letter_thief (#750)
    * feat(gem): letter_thief
    
    letter_opener + letter_opener_web working on Heroku
    
    * feat(gem update): bundler
    
    --> 4.0.4
    
    * fix(gemfile): stringio removal
    
    * fix(gem stringio): fixing version to 3.2.0
    
    * fix(gemfile and ci tests): back to older version of bundler
    
    * fix(gemfile): stringio specifying version
    
    * fix(gemfile): stringio downgrading to 3.1.2
    
    * fix(gemfile): stringio removal
    
    supposed to be included with rails
    
    * fix(circle ci): bundler version --> 4.0.4
    
    * fix(circle ci config ): updating bundler at the right place
    
    * fix(circleci config): forcing bundler version
    
    * fix(circleci bundler installation in config file): commands reordered
    
    * Enforce Bundler 2.5.23
    
    * fix(bundler version): it is to be 2.5.23
    
    * fix(config): circleci bundler update
    
    * fix(scope LetterThief): not on production nor on staging
    
    * fix(circleci config): using bundler version
    
    * fix(script for bundling): needs to use a BUNDLER_VERSION in the env variables
    
    * fix(config): bundler : using 2.6.9 in
    
    * fix(config) :circle ci
    
    * fix(gemfile): using preinstalled gem version : fiddle : 1.1.6
    
    * fix(ci): back to ruby 3.3.6
    
    * fix(gemfile): stringio : 3.20
    
    * fix(config) : circle ci and bundler --> back to 2.5.23
    
    * fix(config):circle ci + ruby verfsion back to 3.4.7
    
    * fix(gemfile): adding letter_thief for review env
    
    * fix(config): circleci : refering to existing review file for no change
    
    * feat(rails update): 7.1 --> 7.2
    
    * fix(gemfile and bundle): adding extra gems and updating bundle to not have its version in a var env
    
    * fix(deployment on Heroku): letter_thief withdrawal . Giving pup
    
    * fix(bug): review no longer concerned with letter_thief

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_thief
   config.action_mailer.perform_deliveries = true
   config.action_mailer.preview_paths << "#{Rails.root}/lib/mailer_previews"
 
