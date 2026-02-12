d20b0b23ac342030b7b4dad5616c79c78649bed1
Etienne Weil <weil.etienne@hotmail.fr>
2026-02-12 14:53:42 +0100

    fix(code): testing stub with a block

--- test/jobs/send_sms_job_test.rb
+++ test/jobs/send_sms_job_test.rb
@@ -13,10 +13,11 @@ class SendSmsJobTest < ActiveJob::TestCase
     Services::SmsSender.stub_any_instance(:perform, 'ok') do
       assert_equal 'ok', SendSmsJob.perform_now(user: user, message: message)
     end
-    Services::SmsSender.new(phone_number: user.phone, content: message).stub(:perform, 'ok') do
-      computed_phone_number = '33601020304'
-      stub_sms(computed_phone_number)
-      assert_equal true, SendSmsJob.perform_now(user: user, message: message)
+    computed_phone_number = '33601020304'
+    stub_sms(computed_phone_number) do
+      Services::SmsSender.new(phone_number: user.phone, content: message).stub(:perform, 'ok') do
+        assert_equal true, SendSmsJob.perform_now(user: user, message: message)
+      end
     end
   end
 
@@ -26,12 +27,13 @@ class SendSmsJobTest < ActiveJob::TestCase
     phone_number = '+2620601020304'
     user = create(:user, phone: phone_number)
     # parameters = {phone_number: user.phone, content: message}
-    stub_sms(computed_phone_number)
-    Services::SmsSender.stub_any_instance(:perform, 'ok') do
-      assert_equal 'ok', SendSmsJob.perform_now(user: user, message: message)
-    end
-    Services::SmsSender.new(phone_number: user.phone, content: message).stub(:perform, 'ok') do
-      assert SendSmsJob.perform_now(user: user, message: message)
+    stub_sms(computed_phone_number) do
+      Services::SmsSender.stub_any_instance(:perform, 'ok') do
+        assert_equal 'ok', SendSmsJob.perform_now(user: user, message: message)
+      end
+      Services::SmsSender.new(phone_number: user.phone, content: message).stub(:perform, 'ok') do
+        assert SendSmsJob.perform_now(user: user, message: message)
+      end
     end
   end
 end
