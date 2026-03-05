a110095ff01ebe376ed03010a8716a2473e3cd9d
Etienne WEIL <fitchMitch@users.noreply.github.com>
2026-03-05 14:27:51 +0100

    MGF-1527-api-generer-une-erreur-pour-les-offres-colleges-lycees (#760)
    * fix(structure.sql): comments now 'on'
    * fix(seed): api offers now college xor lycee, not both
    * fix(potential bug): in case of inconsistent data you could drag a nil inside grades
    * style(move api admin lines to a concern):
    * docs(api): forbids college and lyces offers
    * feat(Grade): college and lycee grades now defined
    * fix(tests): taking into account api offers are either 'college' or 'lycee'
    * fix(after chatGPT review): some changes
    * fix(api v2): when seconde and troisieme : error duely processed and tested
    
    * fix(code): controller returns error when grades aim college and lycee
    
    * fix(test): obsolete test is skipped

--- test/models/internship_offers/doubling_test.rb
+++ test/models/internship_offers/doubling_test.rb
@@ -1,21 +1,20 @@
 require 'test_helper'
 
 class DoublingOffersTest < ActiveSupport::TestCase
-
   test 'retrofit:doubling_offers with multiple grades and applications of both grades' do
     travel_to Date.new(2023, 10, 1) do
-      #setup
+      # setup
       internship_offer = create(:weekly_internship_offer,
                                 :both_school_tracks_internship_offer,
                                 max_candidates: 3,
-                                grades: [Grade.seconde] + Grade.troisieme_et_quatrieme,
-                                weeks: [Week.seconde_weeks.first] + Week.where(year: 2024, number: [1,2]).to_a
+                                grades: [ Grade.seconde ] + Grade.troisieme_et_quatrieme,
+                                weeks: [ Week.seconde_weeks.first ] + Week.where(year: 2024, number: [ 1, 2 ]).to_a
                                 )
       original_created_at = internship_offer.created_at
       original_updated_at = internship_offer.updated_at
       lycee_school         = create(:school, :lycee, :at_paris)
       college_school       = create(:school, :college, :at_bordeaux)
-      internship_offer.schools = [lycee_school, college_school]
+      internship_offer.schools = [ lycee_school, college_school ]
       student_seconde       = create(:student, :female, grade: Grade.seconde)
       student_troisieme     = create(:student, :male, grade: Grade.troisieme)
       application_seconde   = create(:weekly_internship_application, internship_offer: internship_offer, student: student_seconde)
@@ -34,19 +33,19 @@ class DoublingOffersTest < ActiveSupport::TestCase
       assert_equal 0, internship_offer.stats.total_female_approved_applications_count
 
       assert_equal 2, internship_offer.schools.count
-      assert_equal "2024/01/01", internship_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/06/21", internship_offer.last_date.strftime("%Y/%m/%d")
+      assert_equal '2024/01/01', internship_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/06/21', internship_offer.last_date.strftime('%Y/%m/%d')
 
 
       internship_offer.split_offer
 
 
       internship_offer.reload
-      assert_equal "2024/06/17", internship_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/06/21", internship_offer.last_date.strftime("%Y/%m/%d")
-      assert_equal [Grade.seconde.id], internship_offer.grades.ids
-      assert_equal [application_seconde.id], internship_offer.internship_applications.to_a.map(&:id)
-      assert_equal [Week.seconde_weeks.first.id], internship_offer.weeks.ids
+      assert_equal '2024/06/17', internship_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/06/21', internship_offer.last_date.strftime('%Y/%m/%d')
+      assert_equal [ Grade.seconde.id ], internship_offer.grades.ids
+      assert_equal [ application_seconde.id ], internship_offer.internship_applications.to_a.map(&:id)
+      assert_equal [ Week.seconde_weeks.first.id ], internship_offer.weeks.ids
       assert_equal 1, internship_offer.favorites.count
       assert_equal student_seconde.id, internship_offer.favorites.first.user.id
       assert_equal 3, internship_offer.stats.remaining_seats_count
@@ -66,11 +65,11 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
       new_offer = InternshipOffer.find_by(mother_id: internship_offer.id)
       assert_not_nil new_offer
-      assert_equal "2024/01/01", new_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/01/12", new_offer.last_date.strftime("%Y/%m/%d")
+      assert_equal '2024/01/01', new_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/01/12', new_offer.last_date.strftime('%Y/%m/%d')
       assert_equal Grade.troisieme_et_quatrieme.ids, new_offer.grades.map(&:id)
-      assert_equal [application_troisieme.id], new_offer.internship_applications.to_a.map(&:id)
-      assert_equal Week.where(year: 2024, number: [1,2]).to_a.map(&:id), new_offer.weeks.ids
+      assert_equal [ application_troisieme.id ], new_offer.internship_applications.to_a.map(&:id)
+      assert_equal Week.where(year: 2024, number: [ 1, 2 ]).to_a.map(&:id), new_offer.weeks.ids
       assert_equal 1, new_offer.favorites.count
       assert_equal student_troisieme.id, new_offer.favorites.first.user.id
       assert_equal 3, new_offer.stats.remaining_seats_count
@@ -92,18 +91,18 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
   test 'retrofit:doubling_offers with multiple grades and applications of both grades when offer is multi' do
     travel_to Date.new(2023, 10, 1) do
-      #setup
+      # setup
       internship_offer = create(:multi_internship_offer,
                                 :both_school_tracks_internship_offer,
                                 max_candidates: 3,
-                                grades: [Grade.seconde] + Grade.troisieme_et_quatrieme,
-                                weeks: [Week.seconde_weeks.first] + Week.where(year: 2024, number: [1,2]).to_a
+                                grades: [ Grade.seconde ] + Grade.troisieme_et_quatrieme,
+                                weeks: [ Week.seconde_weeks.first ] + Week.where(year: 2024, number: [ 1, 2 ]).to_a
                                 )
       original_created_at = internship_offer.created_at
       original_updated_at = internship_offer.updated_at
       lycee_school         = create(:school, :lycee, :at_paris)
       college_school       = create(:school, :college, :at_bordeaux)
-      internship_offer.schools = [lycee_school, college_school]
+      internship_offer.schools = [ lycee_school, college_school ]
       student_seconde       = create(:student, :female, grade: Grade.seconde)
       student_troisieme     = create(:student, :male, grade: Grade.troisieme)
       application_seconde   = create(:weekly_internship_application, internship_offer: internship_offer, student: student_seconde)
@@ -122,18 +121,18 @@ class DoublingOffersTest < ActiveSupport::TestCase
       assert_equal 0, internship_offer.stats.total_female_approved_applications_count
 
       assert_equal 2, internship_offer.schools.count
-      assert_equal "2024/01/01", internship_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/06/21", internship_offer.last_date.strftime("%Y/%m/%d")
+      assert_equal '2024/01/01', internship_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/06/21', internship_offer.last_date.strftime('%Y/%m/%d')
 
 
       internship_offer.split_offer
       internship_offer.reload
 
-      assert_equal "2024/06/17", internship_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/06/21", internship_offer.last_date.strftime("%Y/%m/%d")
-      assert_equal [Grade.seconde.id], internship_offer.grades.ids
-      assert_equal [application_seconde.id], internship_offer.internship_applications.to_a.map(&:id)
-      assert_equal [Week.seconde_weeks.first.id], internship_offer.weeks.ids
+      assert_equal '2024/06/17', internship_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/06/21', internship_offer.last_date.strftime('%Y/%m/%d')
+      assert_equal [ Grade.seconde.id ], internship_offer.grades.ids
+      assert_equal [ application_seconde.id ], internship_offer.internship_applications.to_a.map(&:id)
+      assert_equal [ Week.seconde_weeks.first.id ], internship_offer.weeks.ids
       assert_equal 1, internship_offer.favorites.count
       assert_equal student_seconde.id, internship_offer.favorites.first.user.id
       assert_equal 3, internship_offer.stats.remaining_seats_count
@@ -153,11 +152,11 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
       new_offer = InternshipOffer.find_by(mother_id: internship_offer.id)
       assert_not_nil new_offer
-      assert_equal "2024/01/01", new_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/01/12", new_offer.last_date.strftime("%Y/%m/%d")
+      assert_equal '2024/01/01', new_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/01/12', new_offer.last_date.strftime('%Y/%m/%d')
       assert_equal Grade.troisieme_et_quatrieme.ids, new_offer.grades.map(&:id)
-      assert_equal [application_troisieme.id], new_offer.internship_applications.to_a.map(&:id)
-      assert_equal Week.where(year: 2024, number: [1,2]).to_a.map(&:id), new_offer.weeks.ids
+      assert_equal [ application_troisieme.id ], new_offer.internship_applications.to_a.map(&:id)
+      assert_equal Week.where(year: 2024, number: [ 1, 2 ]).to_a.map(&:id), new_offer.weeks.ids
       assert_equal 1, new_offer.favorites.count
       assert_equal student_troisieme.id, new_offer.favorites.first.user.id
       assert_equal 3, new_offer.stats.remaining_seats_count
@@ -179,20 +178,20 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
   test 'retrofit:doubling_offers with older than a year offer with multiple grades and applications of both grades' do
     travel_to Date.new(2024, 10, 1) do
-      #setup
-      weeks1 = [Week.find_by(number: 25, year: 2024)]
-      weeks2 =  Week.where(year: 2024, number: [1,2]).to_a
+      # setup
+      weeks1 = [ Week.find_by(number: 25, year: 2024) ]
+      weeks2 =  Week.where(year: 2024, number: [ 1, 2 ]).to_a
       weeks = weeks1 + weeks2
       internship_offer = create(:weekly_internship_offer,
                                 :both_school_tracks_internship_offer,
                                 max_candidates: 3,
-                                grades: [Grade.seconde] + Grade.troisieme_et_quatrieme,
-                                weeks: weeks )
+                                grades: [ Grade.seconde ] + Grade.troisieme_et_quatrieme,
+                                weeks: weeks)
       original_created_at = internship_offer.created_at
       original_updated_at = internship_offer.updated_at
       lycee_school         = create(:school, :lycee, :at_paris)
       college_school       = create(:school, :college, :at_bordeaux)
-      internship_offer.schools = [lycee_school, college_school]
+      internship_offer.schools = [ lycee_school, college_school ]
       student_seconde       = create(:student, :female, grade: Grade.seconde)
       student_troisieme     = create(:student, :male, grade: Grade.troisieme)
       application_seconde   = create(:weekly_internship_application, internship_offer: internship_offer, student: student_seconde)
@@ -211,19 +210,19 @@ class DoublingOffersTest < ActiveSupport::TestCase
       assert_equal 0, internship_offer.stats.total_female_approved_applications_count
 
       assert_equal 2, internship_offer.schools.count
-      assert_equal "2024/01/01", internship_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/06/21", internship_offer.last_date.strftime("%Y/%m/%d")
+      assert_equal '2024/01/01', internship_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/06/21', internship_offer.last_date.strftime('%Y/%m/%d')
 
 
       internship_offer.split_offer
 
 
       internship_offer.reload
-      assert_equal "2024/06/17", internship_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/06/21", internship_offer.last_date.strftime("%Y/%m/%d")
-      assert_equal [Grade.seconde.id], internship_offer.grades.ids
-      assert_equal [application_seconde.id], internship_offer.internship_applications.to_a.map(&:id)
-      assert_equal [weeks.first.id], internship_offer.weeks.ids
+      assert_equal '2024/06/17', internship_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/06/21', internship_offer.last_date.strftime('%Y/%m/%d')
+      assert_equal [ Grade.seconde.id ], internship_offer.grades.ids
+      assert_equal [ application_seconde.id ], internship_offer.internship_applications.to_a.map(&:id)
+      assert_equal [ weeks.first.id ], internship_offer.weeks.ids
       assert_equal 0, internship_offer.favorites.count
       assert_equal 3, internship_offer.stats.remaining_seats_count
       assert_equal 1, internship_offer.stats.total_applications_count
@@ -242,10 +241,10 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
       new_offer = InternshipOffer.find_by(mother_id: internship_offer.id)
       assert_not_nil new_offer
-      assert_equal "2024/01/01", new_offer.first_date.strftime("%Y/%m/%d")
-      assert_equal "2024/01/12", new_offer.last_date.strftime("%Y/%m/%d")
+      assert_equal '2024/01/01', new_offer.first_date.strftime('%Y/%m/%d')
+      assert_equal '2024/01/12', new_offer.last_date.strftime('%Y/%m/%d')
       assert_equal Grade.troisieme_et_quatrieme.ids, new_offer.grades.map(&:id)
-      assert_equal [application_troisieme.id], new_offer.internship_applications.to_a.map(&:id)
+      assert_equal [ application_troisieme.id ], new_offer.internship_applications.to_a.map(&:id)
       assert_equal weeks2.map(&:id).sort, new_offer.weeks.ids.sort
       assert_equal 0, new_offer.favorites.count
       assert_equal 3, new_offer.stats.remaining_seats_count
@@ -266,17 +265,18 @@ class DoublingOffersTest < ActiveSupport::TestCase
   end
 
   test 'retrofit:doubling_API offers with multiple grades' do
+    skip 'obsolete since api no longer allows creating offers with both college and lycee grades'
     travel_to Date.new(2024, 10, 1) do
-      #setup
+      # setup
       internship_offer = create(:api_internship_offer,
                                 :both_school_tracks_internship_offer,
                                 max_candidates: 3,
-                                grades: [Grade.seconde] + Grade.troisieme_et_quatrieme,
-                                weeks: [Week.seconde_weeks.first] + Week.where(year: 2025, number: [1,2]).to_a
+                                grades: [ Grade.seconde ] + Grade.troisieme_et_quatrieme,
+                                weeks: [ Week.seconde_weeks.first ] + Week.where(year: 2025, number: [ 1, 2 ]).to_a
                                 )
       lycee_school         = create(:school, :lycee, :at_paris)
       college_school       = create(:school, :college, :at_bordeaux)
-      internship_offer.schools = [lycee_school, college_school]
+      internship_offer.schools = [ lycee_school, college_school ]
       student_seconde       = create(:student, :female, grade: Grade.seconde)
       student_troisieme     = create(:student, :male, grade: Grade.troisieme)
       create(:favorite, internship_offer: internship_offer, user: student_troisieme)
@@ -290,8 +290,8 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
       internship_offer.reload
 
-      assert_equal [Grade.seconde.id], internship_offer.grades.ids
-      assert_equal [Week.seconde_weeks.first.id], internship_offer.weeks.ids
+      assert_equal [ Grade.seconde.id ], internship_offer.grades.ids
+      assert_equal [ Week.seconde_weeks.first.id ], internship_offer.weeks.ids
       assert_equal 1, internship_offer.favorites.count
       assert_equal student_seconde.id, internship_offer.favorites.first.user.id
 
@@ -302,12 +302,12 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
   test 'retrofit:doubling_offers with grade 3e and applications' do
     travel_to Date.new(2024, 10, 1) do
-      #setup
+      # setup
       internship_offer = create(:weekly_internship_offer,
                                 :week_1,
                                 max_candidates: 3,
-                                grades: [Grade.seconde],
-                                weeks: [Week.seconde_weeks.first]
+                                grades: [ Grade.seconde ],
+                                weeks: [ Week.seconde_weeks.first ]
                                 )
       student_seconde_f = create(:student, :female, grade: Grade.seconde)
       student_seconde_m = create(:student, :male, grade: Grade.seconde)
@@ -331,9 +331,9 @@ class DoublingOffersTest < ActiveSupport::TestCase
       end
 
       internship_offer.reload
-      assert_equal [Grade.seconde.id], internship_offer.grades.ids
-      assert_equal [application_seconde_f.id, application_seconde_m.id].sort, internship_offer.internship_applications.to_a.map(&:id).sort
-      assert_equal [Week.seconde_weeks.first.id], internship_offer.weeks.ids
+      assert_equal [ Grade.seconde.id ], internship_offer.grades.ids
+      assert_equal [ application_seconde_f.id, application_seconde_m.id ].sort, internship_offer.internship_applications.to_a.map(&:id).sort
+      assert_equal [ Week.seconde_weeks.first.id ], internship_offer.weeks.ids
       assert_equal 2, internship_offer.favorites.count
       assert_equal 3, internship_offer.stats.remaining_seats_count
       assert_equal 2, internship_offer.stats.total_applications_count
@@ -343,18 +343,17 @@ class DoublingOffersTest < ActiveSupport::TestCase
       assert_equal 1, internship_offer.stats.total_female_applications_count
       assert_equal 0, internship_offer.stats.total_male_approved_applications_count
       assert_equal 0, internship_offer.stats.total_female_approved_applications_count
-
     end
   end
 
   test 'retrofit:doubling_offers fails gracefully when weeks are missing in 3eme grade' do
     travel_to Date.new(2024, 10, 1) do
-      #setup
+      # setup
       internship_offer = create(:weekly_internship_offer,
                                 :week_1,
                                 max_candidates: 3,
-                                grades: [Grade.troisieme, Grade.seconde],
-                                weeks: [Week.seconde_weeks.first]
+                                grades: [ Grade.troisieme, Grade.seconde ],
+                                weeks: [ Week.seconde_weeks.first ]
                                 )
       student_seconde_f = create(:student, :female, grade: Grade.seconde)
       student_seconde_m = create(:student, :male, grade: Grade.seconde)
@@ -381,12 +380,12 @@ class DoublingOffersTest < ActiveSupport::TestCase
 
   test 'retrofit:doubling_offers fails gracefully when weeks are missing in 2nde grade' do
     travel_to Date.new(2024, 10, 1) do
-      #setup
+      # setup
       internship_offer = create(:weekly_internship_offer,
                                 :week_1,
                                 max_candidates: 3,
-                                grades: [Grade.quatrieme, Grade.troisieme, Grade.seconde],
-                                weeks: Week.where(year: 2025, number: [1,2])
+                                grades: [ Grade.quatrieme, Grade.troisieme, Grade.seconde ],
+                                weeks: Week.where(year: 2025, number: [ 1, 2 ])
                                 )
       student_seconde_f = create(:student, :female, grade: Grade.seconde)
       student_seconde_m = create(:student, :male, grade: Grade.seconde)
@@ -411,8 +410,8 @@ class DoublingOffersTest < ActiveSupport::TestCase
         end
       end
       internship_offer.reload
-      assert_equal [Grade.troisieme.id, Grade.quatrieme.id].sort, internship_offer.grades.ids.sort
-      assert_equal [application_seconde_f.id, application_seconde_m.id].sort, internship_offer.internship_applications.to_a.map(&:id).sort
+      assert_equal [ Grade.troisieme.id, Grade.quatrieme.id ].sort, internship_offer.grades.ids.sort
+      assert_equal [ application_seconde_f.id, application_seconde_m.id ].sort, internship_offer.internship_applications.to_a.map(&:id).sort
     end
   end
 end
