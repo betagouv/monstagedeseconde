a35e15e5316f97cd036822d0291ef3e0858dfc9d
Etienne Weil <weil.etienne@hotmail.fr>
2026-04-15 17:53:42 +0200

    feat(MGF-1592): Add legal representative email and full name fields to user identity form
    This commit adds the ability for students to enter their legal representative's email and full name when editing their account information. It also includes tests to ensure that these fields are properly displayed and validated in different scenarios, including when the user is an employer or a student with no legal representative data entered.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,9 +37,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'GET account_path(section: :school) as SchoolManagement' do
     school = create(:school, :with_school_manager)
-    [school.school_manager,
+    [ school.school_manager,
      create(:teacher, school:),
-     create(:other, school:)].each do |role|
+     create(:other, school:) ].each do |role|
       sign_in(role)
       get account_path(section: 'school')
     end
@@ -419,9 +419,44 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     post dismiss_modal_info_path(params: { user: { show_modal_info: false } })
     assert_response :success
     refute employer.reload.show_modal_info
-    
+
     get account_path(section: 'password')
     refute_select('div[data-open-modal-dsfr-is-open-value="true"]', {}, 'info modal should be dismissed')
     ENV['SIGNATURE_INFO'] = 'false'
+  end
+
+  test 'GET account_path(section: :identity) as student shows legal representative fields' do
+    student = create(:student)
+    sign_in(student)
+    get account_path(section: 'identity')
+    assert_response :success
+    assert_template 'users/_edit_identity'
+    assert_select 'input[name="user[legal_representative_email]"]' do |inputs|
+      assert inputs.first['class'].include?('text-grey-disabled'), 'email field should be read-only styled'
+    end
+    assert_select 'input[name="user[legal_representative_full_name]"]' do |inputs|
+      assert_equal 'true', inputs.first['readonly'], 'full name field should be readonly'
+      assert inputs.first['class'].include?('text-grey-disabled'), 'full name field should be read-only styled'
+    end
+  end
+
+  test 'GET account_path(section: :identity) as student shows legal representative data' do
+    student = create(:student,
+                     legal_representative_email: 'parent@example.com',
+                     legal_representative_full_name: 'Jean Dupont')
+    sign_in(student)
+    get account_path(section: 'identity')
+    assert_response :success
+    assert_select 'input[name="user[legal_representative_email]"][value="parent@example.com"]'
+    assert_select 'input[name="user[legal_representative_full_name]"][value="Jean Dupont"]'
+  end
+
+  test 'GET account_path(section: :identity) as employer does not show legal representative fields' do
+    employer = create(:employer)
+    sign_in(employer)
+    get account_path(section: 'identity')
+    assert_response :success
+    assert_select 'input[name="user[legal_representative_email]"]', false
+    assert_select 'input[name="user[legal_representative_full_name]"]', false
   end
 end
