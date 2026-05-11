67e6538bf7b39bff59f20921fd08ec8556a3ad20
pierrax <maxime.pierrot@gmail.com>
2026-05-11 16:13:15 +0200

    Fix tests

--- app/libs/dto/multi_planning_adapter.rb
+++ app/libs/dto/multi_planning_adapter.rb
@@ -1,6 +1,7 @@
 module Dto
   class MultiPlanningAdapter < PlanningAdapter
-    
+    def join_association_name = :multi_planning_weeks
+
     def manage_planning_associations
       manage_grades
       manage_weeks
