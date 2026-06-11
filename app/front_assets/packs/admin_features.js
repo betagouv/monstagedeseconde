5d94455c300c48c703316bd1e95183e64e4ae0af
Etienne Weil <weil.etienne@hotmail.fr>
2026-06-11 17:58:59 +0200

    fix(secondary search) : no debounce just click for searching

--- app/front_assets/packs/admin_features.js
+++
@@ -1,5 +0,0 @@
-import { Application } from "@hotwired/stimulus";
-import AdminSearchController from "../controllers/admin_search_controller";
-
-const application = Application.start();
-application.register("admin-search", AdminSearchController);
