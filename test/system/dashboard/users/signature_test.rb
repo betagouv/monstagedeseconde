cba25b713305e1f2edf675bfc648a0d476ff6af2
pierrax <maxime.pierrot@gmail.com>
2026-07-23 17:44:12 +0200

    Fix flaky group signing test (order-dependent assertions)

--- test/system/dashboard/users/signature_test.rb
+++ test/system/dashboard/users/signature_test.rb
@@ -297,19 +297,15 @@ module Dashboard
         end
 
         assert_equal 2, Signature.all.count
-        signatures = Signature.all.order(:id)
 
-        signature = signatures.first
-        assert_equal internship_agreement.id, signature.internship_agreement.id
-        assert_equal school_manager.id, signature.school_manager.id
-        assert_equal DateTime.now, signature.signature_date
-        assert_equal 'school_manager', signature.signatory_role
-
-        signature = signatures.last
-        assert_equal internship_agreement_2.id, signature.internship_agreement.id
-        assert_equal school_manager.id, signature.school_manager.id
-        assert_equal DateTime.now, signature.signature_date
-        assert_equal 'school_manager', signature.signatory_role
+        # Signatures are not created in a deterministic order: look them up per agreement
+        [internship_agreement, internship_agreement_2].each do |agreement|
+          signature = Signature.find_by(internship_agreement_id: agreement.id)
+          assert_not_nil signature
+          assert_equal school_manager.id, signature.school_manager.id
+          assert_equal DateTime.now, signature.signature_date
+          assert_equal 'school_manager', signature.signatory_role
+        end
 
 
         find('h1', text: 'Éditer, imprimer et signez vos conventions dématérialisées')
@@ -355,19 +351,15 @@ module Dashboard
         end
 
         assert_equal 2, Signature.all.count
-        signatures = Signature.all.order(:id)
 
-        signature = signatures.first
-        assert_equal internship_agreement.id, signature.internship_agreement.id
-        assert_equal school_manager.id, signature.school_manager.id
-        assert_equal DateTime.now, signature.signature_date
-        assert_equal 'school_manager', signature.signatory_role
-
-        signature = signatures.last
-        assert_equal internship_agreement_2.id, signature.internship_agreement.id
-        assert_equal school_manager.id, signature.school_manager.id
-        assert_equal DateTime.now, signature.signature_date
-        assert_equal 'school_manager', signature.signatory_role
+        # Signatures are not created in a deterministic order: look them up per agreement
+        [internship_agreement, internship_agreement_2].each do |agreement|
+          signature = Signature.find_by(internship_agreement_id: agreement.id)
+          assert_not_nil signature
+          assert_equal school_manager.id, signature.school_manager.id
+          assert_equal DateTime.now, signature.signature_date
+          assert_equal 'school_manager', signature.signatory_role
+        end
 
 
         find('h1', text: 'Éditer, imprimer et signez vos conventions dématérialisées')
