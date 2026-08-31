2cd009222cc88994c64817a3e24c9c73d9d44c82
pierrax <maxime.pierrot@gmail.com>
2026-08-31 14:48:37 +0200

    Switch offer detail maps to OSM France tiles

--- app/front_assets/components/Map.jsx
+++ app/front_assets/components/Map.jsx
@@ -28,7 +28,7 @@ const Map = ({ internshipOffer }) => {
         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
           />
           <MarkerClusterGroup>
             <Marker
